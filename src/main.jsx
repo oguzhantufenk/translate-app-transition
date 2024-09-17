23ee5f01dac388df00d2a6d5d1eccc44dfc8fe33
Oğuzhan Tüfenk <117628314+oguzhannftsiblings@users.noreply.github.com>
2024-09-17 19:36:46 +0300

    Correct naming of App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ThreadsModal from "./App.jsx";
+import App from "./App.jsx";
+
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThreadsModal />
+    <App />
   </StrictMode>
 );
